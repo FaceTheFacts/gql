2d5cd545e8913c3ce13914072bd661f10209b829
Kevin Gao <k_gao@aol.com>
2022-02-25 23:54:51 +0100

    chore: remove deprecated && ignore /dist

--- src/helpers/fetchEntities.ts
+++
@@ -1,9 +0,0 @@
-// @ts-nocheck
-import { loadEntities } from "./loadEntities";
-
-const main = async (): Promise<void> => {
-  const apiParties = await loadEntities("parties");
-  console.log(apiParties);
-};
-
-main();
