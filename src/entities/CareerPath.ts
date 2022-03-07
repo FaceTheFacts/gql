3a0a271b3ae89edf9b87426fe44683652c00bab0
Kevin Gao <k_gao@aol.com>
2022-03-07 15:11:51 +0100

    refactor: CareerPath

--- src/entities/CareerPath.ts
+++ src/entities/CareerPath.ts
@@ -1,28 +1,21 @@
-import {
-  Column,
-  Entity,
-  JoinColumn,
-  ManyToOne,
-  PrimaryGeneratedColumn,
-} from "typeorm";
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
 
 import { Cv } from "./Cv";
 
-@Entity("career_path", { schema: "public" })
+@Entity()
 export class CareerPath {
-  @PrimaryGeneratedColumn({ type: "integer", name: "id" })
+  @PrimaryGeneratedColumn()
   public id: number;
 
   @Column("text", { nullable: true })
-  public rawText: string;
+  public rawText?: string;
 
-  @Column("text", { name: "label", nullable: true })
+  @Column("text", { nullable: true })
   public label?: string;
 
-  @Column("varchar", { name: "period", nullable: true })
+  @Column("varchar", { nullable: true })
   public period?: string;
 
   @ManyToOne(() => Cv, (cv) => cv.careerPaths)
-  @JoinColumn([{ name: "cv_id", referencedColumnName: "id" }])
   public cv: Cv;
 }
